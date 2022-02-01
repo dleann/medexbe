22473c1181d5a831def70ab44f707cf52cc62b13
dleann <dinaleanng@gmail.com>
2022-01-31 20:58:55 -0600

    Terminacion de crud en Expedientes

--- routes/api/v1/expedientes/expedientes.js
+++ routes/api/v1/expedientes/expedientes.js
@@ -1,33 +1,87 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.status(200).json({
-    endpoint: "expedientes",
-    updates: new Date(2022, 0, 22, 18, 41, 00),
-  });
+
+const Expedientes = new require('../../../../dao/expedientes/expedientes.model');
+const expedienteModel = new Expedientes();
+
+router.get('/', (req, res) => {
+    res.status(200).json({
+        endpoint: 'Expedientes',
+        updates: new Date(2022, 0, 19, 18, 41, 00)
+    });
+}); //GET /
+
+router.get('/all', async(req, res) => {
+    try {
+        const rows = await expedienteModel.getAll();
+        res.status(200).json({ status: 'ok', expedientes: rows });
+    } catch (ex) {
+        console.log(ex);
+        res.status(500).json({ status: 'failed' });
+    }
 });
-//Get/
-router.post("/new", async (req, res) => {
-  const {
-    identidad,
-    Fecha,
-    Descripcion,
-    Observacion,
-    registros,
-    UltimaActualizacion,
-  } = req.body;
-  res.status(200).json({
-    status: "ok",
-    recieved: {
-      identidad,
-      Fecha,
-      Descripcion,
-      Observacion,
-      registros,
-      UltimaActualizacion,
-    },
-  });
+// /byid/1;
+router.get('/byid/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const row = await expedienteModel.getById(parseInt(id));
+        res.status(200).json({ status: 'ok', expediente: row });
+    } catch (ex) {
+        console.log(ex);
+        res.status(500).json({ status: 'failed' });
+    }
 });
-//POST/new
+
+
+
+router.post('/new', async(req, res) => {
+    const { identidad, fecha, descripcion, observacion, resgistros, ultimaActualizacion } = req.body;
+    try {
+        rslt = await expedienteModel.new(identidad, fecha, descripcion, observacion, resgistros, ultimaActualizacion);
+        res.status(200).json({
+            status: 'ok',
+            result: rslt
+        });
+    } catch (ex) {
+        console.log(ex);
+        res.status(500).json({
+            status: 'failed',
+            result: {}
+        });
+    }
+}); //POST /new
+
+
+//router.put();
+router.put('/update/:id', async(req, res) => {
+    try {
+        const { identidad, fecha, descripcion, observacion, resgistros, ultimaActualizacion } = req.body;
+        const { id } = req.params;
+        const result = await expedienteModel.updateOne(id, identidad, fecha, descripcion, observacion, resgistros, ultimaActualizacion);
+        res.status(200).json({
+            status: 'ok',
+            result
+        });
+    } catch (ex) {
+        console.log(ex);
+        res.status(500).json({ status: 'failed' });
+    }
+});
+
+//router.delete();
+router.delete('/delete/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await expedienteModel.deleteOne(id);
+        res.status(200).json({
+            status: 'ok',
+            result
+        });
+    } catch (ex) {
+        console.log(ex);
+        res.status(500).json({ status: 'failed' });
+    }
+});
+
 
 module.exports = router;
