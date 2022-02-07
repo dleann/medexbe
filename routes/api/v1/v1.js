4400b081991288c9cf3e8742cc63150890dfecd2
dleann <dinaleanng@gmail.com>
2022-02-07 13:34:28 -0600

    20220207 middleware

--- routes/api/v1/v1.js
+++ routes/api/v1/v1.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const { verifyApiHeaderToken } = require('./headerVerifyMiddleware');
 const router = express.Router();
 const pacientesRoutes = require('./pacientes/pacientes');
-// const expedientesRoutes = require('./expedientes/expedientes');
+const expedientesRoutes = require('./expedientes/expedientes');
 //middlewares
-router.use('/pacientes', pacientesRoutes);
-// router.use('/expedientes', expedientesRoutes);
+router.use(
+    '/pacientes',
+    verifyApiHeaderToken, 
+    pacientesRoutes
+);
+router.use('/expedientes', expedientesRoutes);
 module.exports = router;
